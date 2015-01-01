e500999818e7247b6ee2a6017d7c0aad74571b8d
Marcelo Emanoel B. Diniz <marceloemanoel@gmail.com>
2015-01-01 17:45:54 -0300

    Rename stage.js to Stage.js

--- games/azureStar/Stage.js
+++ games/azureStar/Stage.js
@@ -1,28 +1,31 @@
-function Stage(imageURL, name,exibitionArea, speed) {
+var Stage = function(imageURL, name,exibitionArea, speed) {
+  var self = this;
+
   this.backgroundImage = new Image();
   this.currentHeight;
   this.exibitionArea = exibitionArea;
   this.speed = speed;
   this.isReady = false;
   this.name = name;
-  var self = this;
   this.backgroundImage.onload = function () {
-	   self.isReady = true;
-	   self.currentHeight = self.backgroundImage.height - self.exibitionArea
+    self.isReady = true;
+    self.currentHeight = self.backgroundImage.height - self.exibitionArea
   };
   this.backgroundImage.src = imageURL;
 };
 
 Stage.prototype.build = function (context) {
-	if (this.isReady) {
-		context.drawImage(this.backgroundImage, 0, -this.currentHeight, this.backgroundImage.width, this.backgroundImage.height);
-		if (this.currentHeight >= this.speed) {
-			this.currentHeight -= this.speed;
-		}
+  if (this.isReady) {
+    context.drawImage(this.backgroundImage, 0, -this.currentHeight, this.backgroundImage.width, this.backgroundImage.height);
+
+    if (this.currentHeight >= this.speed) {
+      this.currentHeight -= this.speed;
+    }
+
     if ( (this.backgroundImage.height - this.exibitionArea) - this.currentHeight < 120 ) {
       this.showName(context);
     }
-	}
+  }
 };
 
 Stage.prototype.showName = function (context) {
