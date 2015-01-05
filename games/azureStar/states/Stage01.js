e0c2098bca6167532fb114144a3d8e7131e2f97e
Marcelo Emanoel B. Diniz <marceloemanoel@gmail.com>
2015-01-05 13:18:29 -0300

    Starting point for a stage01

--- games/azureStar/states/Stage01.js
+++ games/azureStar/states/Stage01.js
@@ -1,23 +1,93 @@
 var Stage01 = extend(Stage, function(game) {
 
   var self = this,
-      ship = new SpaceShip(game.boundaries);
+      enemies = [],
+      enemyInterval = null,
+      boundaries = game.boundaries(),
+      ship = new SpaceShip(boundaries),
+      movePatterns = new MovePatterns();
 
-  Stage.call(this, 'images/stages/witchBroomNebula.jpg',
-                   'Witch Broom Nebula',
-                   game.boundaries().height,
-                   0.6);
+  Stage.call(
+    this,
+    'images/stages/witchBroomNebula.jpg',
+    'Witch Broom Nebula',
+    boundaries.height,
+    0.6
+  );
+
+  function deployEnemies(enemyFactory) {
+    return function(amount) {
+      var handler = setInterval(function() {
+        if(amount == 0) {
+          clearInterval(handler);
+        }
+        enemies.push(enemyFactory());
+        amount--;
+      }, 300)
+    };
+  }
+  // window.setTimeout(function(){
+  //   enemyInterval = setInterval(function () {
+  //       enemies.push(new Enemy(boundaries, {
+  //         image: 'images/enemies/enemy01.png',
+  //         speed: 3,
+  //         movePattern: movePatterns.select()
+  //       }));
+  //     }, 500);
+  // }, 2000);
+
+  this.stairsWave = deployEnemies(function() {
+    return new Enemy(boundaries, {
+            image: 'images/enemies/enemy01.png',
+            speed: 4,
+            movePattern: movePatterns.stairs
+           });
+    });
+
+  this.circleWave = deployEnemies(function() {
+    return new Enemy(boundaries, {
+             image: 'images/enemies/enemy01.png',
+             speed: 5,
+             movePattern: movePatterns.circle
+           });
+    });
+  this.bouncingCircleWave = deployEnemies(function(){
+    return new Enemy(boundaries, {
+      image: 'images/enemies/enemy01.png',
+      speed: 5,
+      movePattern: movePatterns.bouncingCircle,
+      position: {
+        x: boundaries.width,
+        y: 30
+      }
+    });
+  });
 
   window.stage = this;
+  window.setTimeout(self.stairsWave, 10000, 20);
+  window.setTimeout(self.circleWave, 1000, 20);
+  window.setTimeout(self.stairsWave, 50000, 20);
 
   this.update = function() {
-    self.parent.update.call(this);
+    this.parent.update.call(this);
+    enemies =
+      enemies.map(function(enemy){
+        enemy.update(ship);
+        return enemy;
+      })
+      .filter(function(enemy) {
+        return enemy.y < boundaries.height;
+      });
+
     ship.update();
   };
 
   this.render = function(context) {
-    self.parent.render.call(this, context);
+    this.parent.render.call(this, context);
     ship.render(context);
+    enemies.forEach(function(enemy) {
+      enemy.render(context);
+    });
   };
 
   this.readInput = function(input) {
@@ -38,16 +108,16 @@ var Stage01 = extend(Stage, function(game) {
     }
   };
 
-  this.addEnemy = function () {
-    var self = this;
-    setInterval(function () {
-      self.enemies.push(new Enemy('images/enemies/enemy01.png'));
-      self.enemies = self.enemies.filter(function (enemy) {
-        return enemy.y < enemy.maxPosition.height;
-      });
-    }, 1000);
-  };
   this.isComplete = function() {
     return false;
   };
+
+  this.resize = function(newBoundaries) {
+    boundaries = newBoundaries;
+    ship.boundaries = newBoundaries;
+    enemies.forEach(function(enemy){
+      enemy.boundaries = newBoundaries;
+    });
+  };
+
 });
